16deda42482910c545e8a1b400ff02f532417fc2
Your Name <you@example.com>
2022-09-27 15:41:04 +0530

    added:Handle Exception if

--- src/main/java/com/moodanalyser/MoodAnalyserMain.java
+++ src/main/java/com/moodanalyser/MoodAnalyserMain.java
@@ -1,9 +1,18 @@
 package com.moodanalyser;
 
 public class MoodAnalyserMain {
+    private String message;
+
     /**
-     * create a constructor ,this is default constructor name as MoodAnalyserMain
+     * create a default constructor name as MoodAnalyserMain
      */
+
+    public MoodAnalyserMain() {
+    }
+
+    public MoodAnalyserMain(String message) {
+        this.message = message;
+    }
     public String analyseMood(String message) {
         if (message.contains("Sad"))
             return "SAD";
--- src/main/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/main/java/com/moodanalyser/MoodAnalyserTest.java
@@ -4,9 +4,15 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 public class MoodAnalyserTest {
         @Test
-        public void given_AnyMood_Should_Return_HAPPY() {
+        public void given_AnyMood_Should_Return_SAD() {
             MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
-            String mood = moodAnalyser.analyseMood("I am in Any Mood");
+            String mood = moodAnalyser.analyseMood("I am in SAD Mood");
             Assert.assertEquals(mood, "HAPPY");
         }
+        @Test
+         public void given_AnyMood_Should_Return_HAPPY() {
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
+        String mood = moodAnalyser.analyseMood("I am in Happy Mood");
+        Assert.assertEquals("HAPPY", mood);
+    }
     }
