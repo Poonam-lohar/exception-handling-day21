054a7a18dcbdda434dbb13e5056e4ace22c8ba01
Your Name <you@example.com>
2022-09-27 16:19:11 +0530

    added:Invalid mood

--- src/main/java/com/moodanalyser/MoodAnalyserMain.java
+++ src/main/java/com/moodanalyser/MoodAnalyserMain.java
@@ -13,10 +13,19 @@ public class MoodAnalyserMain {
     public MoodAnalyserMain(String message) {
         this.message = message;
     }
-    public String analyseMood(String message) {
-        if (message.contains("Sad"))
-            return "SAD";
-        else
-          return "HAPPY";
+    public String analyseMood() throws MoodAnalysisException {
+        try {
+            if (message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (Exception e) {
+            if (message == null)
+                throw new MoodAnalysisException("Please provide valid mood, dont provide null in arguments",
+                        MoodAnalysisException.Exception_Type.NULL);
+            else
+                throw new MoodAnalysisException("Please provide valid mood, dont keep empty in arguments",
+                        MoodAnalysisException.Exception_Type.EMPTY);
+        }
     }
 }
--- src/main/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/main/java/com/moodanalyser/MoodAnalyserTest.java
@@ -1,18 +1,32 @@
 package com.moodanalyser;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 public class MoodAnalyserTest {
-        @Test
-        public void given_AnyMood_Should_Return_SAD() {
-            MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
-            String mood = moodAnalyser.analyseMood("I am in SAD Mood");
-            Assert.assertEquals(mood, "HAPPY");
+    @Test
+    public void given_SadMood_Should_Return_SAD() {
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Sad Mood");
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+            assertSame("SAD", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
         }
-        @Test
-         public void given_AnyMood_Should_Return_HAPPY() {
-        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
-        String mood = moodAnalyser.analyseMood("I am in Happy Mood");
-        Assert.assertEquals("HAPPY", mood);
     }
+
+    @Test
+    public void given_AnyMood_Should_Return_HAPPY() {
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Happy Mood");
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+            assertSame("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
+}
+
